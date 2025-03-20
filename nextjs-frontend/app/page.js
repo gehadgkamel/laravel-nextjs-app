5407916fa7593a78c4f6129b98147c9c12086809
gehadgkamel <gehad.g.kamel22@gmail.com>
2025-03-20 15:45:57 +0200

    add frontend

---
+++ nextjs-frontend/app/page.js
@@ -0,0 +1,34 @@
+"use client";
+import axios from 'axios';
+import { useEffect, useState } from 'react';
+
+export default function Home() {
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    const fetchPosts = async () => {
+      try {
+        const response = await axios.get('http://app:8000/api/posts');
+        setPosts(response.data);
+      } catch (error) {
+        console.error('Error fetching posts:', error);
+      }
+    };
+
+    fetchPosts();
+  }, []);
+
+  return (
+    <div>
+      <h1>Posts</h1>
+      <ul>
+        {posts.map((post) => (
+          <li key={post.id}>
+            <h2>{post.title}</h2>
+            <p>{post.content}</p>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
